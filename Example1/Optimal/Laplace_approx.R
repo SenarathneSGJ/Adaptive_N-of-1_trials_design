1f0bf5959e229a252a32e33e70579fa8a2e2e82f
SenarathneSGJ <SenarathneSGJ@users.noreply.github.com>
2019-09-14 11:12:11 +1000

    Delete Laplace_approx.R

--- Example1/Optimal/Laplace_approx.R
+++
@@ -1,7 +0,0 @@
-Laplace_approx<- function(data.mat,theta,log_postf1,para2,mu_pr,sigma_pr)
-  {
-  #lp.approx <- optim(par=theta,X=X,Z=Z, Y=Y,fn=log_postf1, hessian=TRUE,method="L-BFGS-B",lower=c(apply(prior[,1:5],2,min)),upper=c(apply(prior[,1:5],2,max)))
-  lp.approx <-nlminb(start=theta,data.mat=data.mat,para2=para2,mu_pr=mu_pr,sigma_pr=sigma_pr, objective =log_postf1,lower=c(apply(prior[,1:5],2,min)),upper=c(apply(prior[,1:5],2,max))) #control = list(eval.max=10000,iter.max=10000,sing.tol=1e-18,rel.tol=1e-14),
-  
-  lp.approx
-}
