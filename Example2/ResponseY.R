cef76010ef69f4c3816beb4d59ec043cfe1c644e
Jagath Senarathne <SenarathneSGJ@users.noreply.github.com>
2019-09-18 17:45:06 +1000

    Update ResponseY.R

--- Example2/ResponseY.R
+++ Example2/ResponseY.R
@@ -1,10 +1,6 @@
 ResponseY<- function(X,theta,Patient,b0,b1){
   
   Ey <- (theta[1]+b0)+(theta[2]+b1)*X
-  Ey2=1/(1+exp(-Ey))
-  p=Ey2*exp(theta[3])
-  q=(1-Ey2)*exp(theta[3])
-  
-  Y <- rbeta(length(Ey),p,q)
+  Y <- rnorm(length(Ey),Ey,exp(theta[3]))
   return(Y)
 }
