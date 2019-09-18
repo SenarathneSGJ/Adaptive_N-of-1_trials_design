600b12f5d036d147a94e58096a1cb4c02ab24253
Jagath Senarathne <SenarathneSGJ@users.noreply.github.com>
2019-09-18 17:50:14 +1000

    Update log_posteriorF.R

--- Example2/log_posteriorF.R
+++ Example2/log_posteriorF.R
@@ -3,8 +3,8 @@ log_postF <- function(data.mat,theta,para2,mu_pr,sigma_pr)
     log.prior<- dmvnorm(x=theta[1:5],mean=mu_pr,sigma=sigma_pr,log=TRUE)
     log.like <- sum(LogLikeF(data.mat=data.mat,para=theta,para2=para2,mu_pr=mu_pr,sigma_pr=sigma_pr))
 	
-	log.like[is.na(log.like) | log.like== Inf | log.like == -Inf] <- -3e+2
+    log.like[is.na(log.like) | log.like== Inf | log.like == -Inf] <- -3e+2
     Neg_log_post=-1*(log.prior + log.like)
-    #print(Neg_log_post)
+    
     return(Neg_log_post)
   }
